f78681f62b196f0ea59ad4ccdbeaffe9b8066e0f
Alexandru Danila <alex.danila94@gmail.com>
2018-06-28 14:49:38 -0400

    Add attempt to connect to phone if not connected

--- scripts/bluelock.sh
+++ scripts/bluelock.sh
@@ -2,13 +2,26 @@
 
 export DISPLAY=:0
 
-THRESHOLD=-12
-RSSI=`hcitool rssi 40:CB:C0:03:33:D5 2>/dev/null| egrep -o "[-]?[0-9]+"`
+MAC=40:CB:C0:03:33:D5
+THRESHOLD=-15
 
+
+RSSI=`hcitool rssi $MAC 2>/dev/null| egrep -o "[-]?[0-9]+"`
+if [[ $RSSI = "" ]]; then
+    # bluetoothctl
+bluetoothctl >/dev/null 2>&1 << EOF
+connect $MAC
+EOF
+    # end bluetoothctl
+fi
+
+
+RSSI=`hcitool rssi $MAC 2>/dev/null| egrep -o "[-]?[0-9]+"`
 if [[ $RSSI = "" ]]; then
     echo "Not connected."
     exit 0
 fi
+
 
 if [[ $RSSI -lt $THRESHOLD ]]; then
     if !(gnome-screensaver-command -q 2>/dev/null | grep "is active"); then
@@ -17,5 +30,5 @@ if [[ $RSSI -lt $THRESHOLD ]]; then
 fi
 
 echo "RSSI: $RSSI"
-
+exit 0
 

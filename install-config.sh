3a204674429255a17c17a271c2b010295923bf6b
Alexandru Danila <eadanila@users.noreply.github.com>
2021-03-08 11:58:26 -0500

    Update install-config.sh

--- install-config.sh
+++ install-config.sh
@@ -4,24 +4,24 @@
 # TODO: create a file to autoinstall packages
 # TODO: make it work on macOS
 
-NIXENV=$HOME/Developer/nixenv
+REPO=$HOME/Developer/configs
 CONFIG=$HOME/.config
 
-ln $NIXENV/dotfiles/zshrc $HOME/.zshrc
+ln $REPO/dotfiles/zshrc $HOME/.zshrc
 chsh -s `which zsh`
 
 mkdir -p $CONFIG/bspwm
 mkdir -p $CONFIG/sxhkd
 mkdir -p $CONFIG/polybar
 
-ln $NIVENV/bspwm/bspwmrc $CONFIG/bspwm/bspwmrc
-ln $NIXENV/sxhkd/sxhdkrc $CONFIG/sxhkd/sxhkdrc
-ln $NIXENV/polybar/config $CONFIG/polybar/config
+ln $REPO/bspwm/bspwmrc $CONFIG/bspwm/bspwmrc
+ln $REPO/sxhkd/sxhdkrc $CONFIG/sxhkd/sxhkdrc
+ln $REPO/polybar/config $CONFIG/polybar/config
 
 mkdir -p $CONFIG/nvim/autoload
 curl -fLo $CONFIG/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln $NIXENV/dotfiles/vimrc $CONFIG/nvim/init.vim
+ln $REPO/dotfiles/vimrc $CONFIG/nvim/init.vim
 
 # install plugins and quit
 vim -c PlugInstall -c qall!
